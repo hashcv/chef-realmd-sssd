73c1b976bca562d4ee07a8a32b613a4c7174e6af
John Bartko <john.bartko@drillinginfo.com>
2016-01-11 11:44:22 -0600

    update metadata for supermarket release

--- metadata.rb
+++ metadata.rb
@@ -2,9 +2,11 @@ name             'realmd-sssd'
 maintainer       'John Bartko'
 maintainer_email 'jbartko@gmail.com'
 license          'Apache 2.0'
-description      'Installs/Configures realmd-sssd'
-long_description 'Installs/Configures realmd-sssd'
-version          '0.1.0'
+description      'Installs/Configures realmd and sssd'
+long_description 'Installs/Configures realmd and sssd for use with a Microsoft Active Directory/Kerberos realm'
+source_url       'https://github.com/jbartko/chef-realmd-sssd' if respond_to?(:source_url)
+issues_url       'https://github.com/jbartko/chef-realmd-sssd/issues' if respond_to?(:issues_url)
+version          IO.read(File.join(File.dirname(__FILE__), 'VERSION')) rescue '0.0.1'
 
 depends 'apt'
 depends 'chef-vault'
